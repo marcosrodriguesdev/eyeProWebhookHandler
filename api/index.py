7599745167357fa3166ffccf44074fb7ac033906
marcosrodrigues <marcosrodrigues.sistemas@gmail.com>
2025-05-22 16:27:13 -0300

    correção de caminho do templates

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="api/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
